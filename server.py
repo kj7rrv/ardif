e33ecb5e75b985beadecd7a4442a3b53ac283105
scoopgracie <scoopgracie@gmail.com>
2020-10-23 17:46:03 -0700

    Use Jinja
    No more HTML in server.py!

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import tempfile
 import os
@@ -10,95 +10,7 @@ import cv2
 
 @app.route('/')
 def home():
-    return '''<!DOCTYPE html>
-<html>
-    <head>
-        <title>ARDIF</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-#imagetoardif label {
-    width: 7em;
-    display: inline-block;
-}
-#image label {
-    width: 12em;
-}
-select {
-    margin: 0px;
-    padding: 0px;
-}
-form, form * {
-    background-color: #444444;
-}
-input {
-    background-color: #555555;
-    border: none;
-    margin: 2px;
-}
-button {
-    background-color: #555555;
-    border: none;
-    margin: 5px;
-    box-shadow: 0px 0px 5px black;
-}
-button:hover {
-    box-shadow: 0px 0px 3px black;
-}
-* {
-    font-family: monospace;
-    color: white;
-    background-color: #222222;
-}
-        </style>
-    </head>
-    <body>
-        <h1>ARDIF Converter</h1>
-        <section id=imagetoardif>
-            <h2>Image to ARDIF</h2>
-            <form method=POST action="/image_to_ardif" enctype="multipart/form-data">
-                <fieldset id=callsigns>
-                    <legend>Callsigns</legend>
-                    <label for=sender>Sender: </label><input type=text name=sender id=sender><br>
-                    <label for=recipient>Recipient: </label><input type=text name=recipient id=recipient><br>
-                </fieldset>
-                <fieldset id=info>
-                    <legend>Information</legend>
-                    <label for=title>Title: </label><input type=text name=title id=title><br>
-                    <label for=comment>Comment: </label><input type=text name=comment id=comment><br>
-                </fieldset>
-                <fieldset id=image>
-                    <legend>Image</legend>
-                    <label for=image>File: </label><input type=file name=image id=image><br>
-                    <label for=max_size>Maximum dimension: </label><input type=number value=0 name=max_size id=max_size><br>
-                    <label for=shades>Shades of gray: </label>
-                    <select name=shades id=shades>
-                        <option value=64>64</option>
-                        <option value=32>32</option>
-                        <option value=16>16</option>
-                        <option value=8>8</option>
-                        <option value=4>4</option>
-                        <option value=2>B&W</option>
-                    </select>
-                </fieldset>
-                <button>Convert</button>
-            </form>
-        </section>
-        <section>
-            <h2>ARDIF to Image</h2>
-            <form method=POST action="/ardif_to_image">
-                <label for=ardif>ARDIF: </label><input type=text id=ardif name=ardif><br>
-                <button>Convert</button>
-            </form>
-        </section>
-        <script>
-            sender = document.querySelector('#sender')
-            sender.value = localStorage.getItem('mycallsign') || ''
-            sender.addEventListener('change', function() {
-                localStorage.setItem('mycallsign', sender.value)
-            })
-        </script>
-    </body>
-</html>'''
+    return render_template('index.html')
 
 ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'gif', 'png', 'bmp', 'tif', 'tiff']
 def allowed_file(filename):
@@ -124,53 +36,8 @@ def image_to_ardif():
                     '2': 32,
                     }
             ardif_data = ardif.create_message(path, request.form['sender'], request.form['recipient'], request.form['title'], request.form['comment'], color_divisors[request.form['shades']])
+        return render_template('image_to_ardif.html', data_len=len(ardif_data), data=ardif_data)
         
-        return f'''<!DOCTYPE html>
-<html>
-    <head>
-        <title>ARDIF</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-* {{
-    font-family: sans-serif;
-    color: white;
-    background-color: #222222;
-    font-family: monospace;
-}}
-button {{
-    background-color: #444444;
-    border: none;
-    margin: 5px;
-    box-shadow: 0px 0px 5px black;
-}}
-button:hover {{
-    box-shadow: 0px 0px 3px black;
-}}
-        </style>
-    </head>
-    <body>
-        <h1>ARDIF Converter</h1>
-        <p>Length {len(ardif_data)} bytes.</p>
-        <input type=text id=data value="{ardif_data}">
-        <button id=copy style='display: none'>Copy to clipboard</button><br>
-        <a href=/>Home</a>
-        <script>
-button = document.querySelector('#copy')
-input = document.querySelector('#data')
-button.style = ''
-input.style = 'display: none'
-button.addEventListener('click', function(e) {{
-    e.preventDefault()
-    input.style = ''
-    input.select()
-    input.setSelectionRange(0, 9999999999999999)
-    document.execCommand("copy")
-    input.style = 'display: none'
-    }}
-)
-        </script>
-    </body>
-</html>'''
 
 @app.route('/ardif_to_image', methods=['POST'])
 def ardif_to_image():
@@ -180,74 +47,18 @@ def ardif_to_image():
         path = os.path.join(tmp, 'img.png')
         cv2.imwrite(path, data['image'])
         uri = datauri.DataURI.from_file(path)
-    next_part = ''
     if not data['is_complete']:
         portion_received_escaped = portion_received.replace('\\', '\\\\').replace('"', '\\"')
-        next_part = f'''
-<h2>Next portion</h2>
-<form method=post action="/ardif_to_image">
-    <input type=text name=ardif>
-    <button>Convert</button>
-    <input type=hidden name=previous value="{portion_received_escaped}">
-    </form>'''
-    return f'''<!DOCTYPE html>
-<html>
-    <head>
-        <title>ARDIF</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-table {{
-    border-collapse: collapse;
-    background-color: #444444;
-}}
-th, td {{
-    margin-right: 0px;
-    margin-left: 0px;
-    padding: 3px;
-}}
-th {{
-    border-right: 2px solid white;
-    text-align: right;
-}}
-img {{
-    display: block;
-    padding: 3px;
-}}
-input {{
-    background-color: #555555;
-    border: none;
-    margin: 2px;
-}}
-button {{
-    background-color: #555555;
-    border: none;
-    margin: 5px;
-    box-shadow: 0px 0px 5px black;
-}}
-button:hover {{
-    box-shadow: 0px 0px 3px black;
-}}
-* {{
-    font-family: sans-serif;
-    color: white;
-    background-color: #222222;
-    font-family: monospace;
-}}
-        </style>
-    </head>
-    <body>
-        <h1>ARDIF Converter</h1>
-        <table>
-            <tr><th>Sender</th><td>{data['sender']}</td></tr>
-            <tr><th>Recipient</th><td>{data['recipient']}</td></tr>
-            <tr><th>Date</th><td>{data['date']} UTC</td></tr>
-            <tr><th>Title</th><td>{data['title']}</td></tr>
-            <tr><th>Comment</th><td>{data['comment']}</td></tr>
-        </table>
-        <img src="{uri}"><br>
-        {next_part}
-        <a href=/>Home</a>
-    </body>
-</html>'''
+        next_part = render_template('next_part_form.html', portion_received_escaped=portion_received_escaped)
+    else:
+        next_part = ''
+    return render_template('ardif_to_image.html',
+            sender=data['sender'],
+            recipient=data['recipient'],
+            date=data['date'],
+            title=data['title'],
+            comment=data['comment'],
+            uri=uri,
+            ).replace('__next_part__', next_part)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
